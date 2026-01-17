0efd28d4e0730ccacdb77abd0665289af789b8b8
j.sorg <j.sorg@fz-juelich.de>
2026-01-17 08:18:53 +0100

    code adjusted, because method readValueAs was removed in jackson 3:
    - read array elements in a loop until the end of the array is reached

--- src/main/java/de/fraunhofer/iosb/ilt/sta/jackson/EntityListDeserializer.java
+++ src/main/java/de/fraunhofer/iosb/ilt/sta/jackson/EntityListDeserializer.java
@@ -7,117 +7,103 @@ import tools.jackson.databind.BeanProperty;
 import tools.jackson.databind.DeserializationContext;
 import tools.jackson.databind.JavaType;
 import tools.jackson.databind.ValueDeserializer;
-import tools.jackson.databind.deser.ContextualKeyDeserializer;
 import tools.jackson.databind.deser.std.StdDeserializer;
 import de.fraunhofer.iosb.ilt.sta.model.Entity;
 import static de.fraunhofer.iosb.ilt.sta.model.Entity.AT_IOT_COUNT;
 import static de.fraunhofer.iosb.ilt.sta.model.Entity.AT_IOT_NEXT_LINK;
 import de.fraunhofer.iosb.ilt.sta.model.EntityType;
 import de.fraunhofer.iosb.ilt.sta.model.ext.EntityList;
-import java.io.IOException;
 import java.net.URI;
-import java.util.Iterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EntityListDeserializer<T extends Entity<T>> extends StdDeserializer<EntityList<T>> {
 
-    private static final long serialVersionUID = 8376494553925868647L;
-    private static final Logger LOGGER = LoggerFactory.getLogger(EntityListDeserializer.class);
+	private static final long serialVersionUID = 8376494553925868647L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(EntityListDeserializer.class);
 
-    private Class<T> type;
+	private Class<T> type;
 
-    public EntityListDeserializer() {
-        super(EntityList.class);
-    }
+	public EntityListDeserializer() {
+		super(EntityList.class);
+	}
 
-    public EntityListDeserializer(Class<T> type) {
-        super(EntityList.class);
-        this.type = type;
-    }
+	public EntityListDeserializer(Class<T> type) {
+		super(EntityList.class);
+		this.type = type;
+	}
 
-    @SuppressWarnings("unchecked")
-    @Override
-    public ValueDeserializer<?> createContextual(DeserializationContext ctxt, BeanProperty property) throws JacksonException {
-        final JavaType wrapperType;
-        if (property == null) {
-            wrapperType = ctxt.getContextualType();
-        } else {
-            wrapperType = property.getType();
-        }
+	@SuppressWarnings("unchecked")
+	@Override
+	public ValueDeserializer<?> createContextual(DeserializationContext ctxt, BeanProperty property)
+			throws JacksonException {
+		final JavaType wrapperType;
+		if (property == null) {
+			wrapperType = ctxt.getContextualType();
+		} else {
+			wrapperType = property.getType();
+		}
 
-        JavaType valueType = wrapperType.containedType(0);
-        return new EntityListDeserializer(valueType.getRawClass());
-    }
+		JavaType valueType = wrapperType.containedType(0);
+		return new EntityListDeserializer(valueType.getRawClass());
+	}
 
-    @Override
-    public EntityList<T> deserialize(JsonParser parser, DeserializationContext context)
-            throws JacksonException {
+	@Override
+	public EntityList<T> deserialize(JsonParser parser, DeserializationContext context)
+			throws JacksonException {
 
-        final EntityList<T> entities = new EntityList<>(EntityType.listForClass(type));
-        
-        JsonToken currentToken = parser.currentToken();
-        if (currentToken == JsonToken.START_ARRAY) {
-            // Direct array, probably expanded.
-            JsonToken nextToken = parser.nextToken();
-            if (nextToken != JsonToken.END_ARRAY) {
-                Iterator<T> readValuesAs = parser.readValuesAs(type);
-                while (readValuesAs.hasNext()) {
-                    T next = readValuesAs.next();
-                    entities.add(next);
-                }
-            }
-        } else {
-            boolean done = false;
-            while (!done) {
-                JsonToken mainToken = parser.nextToken();
-                switch (mainToken) {
-                    case END_OBJECT:
-                        done = true;
-                        break;
+		final EntityList<T> entities = new EntityList<>(EntityType.listForClass(type));
 
-                    case PROPERTY_NAME:
-                        String fieldName = parser.currentName();
-                        JsonToken valueToken = parser.nextToken();
-                        switch (fieldName) {
-                            case AT_IOT_COUNT:
-                                entities.setCount(parser.getValueAsLong());
-                                break;
+		JsonToken currentToken = parser.currentToken();
+		if (currentToken == JsonToken.START_ARRAY) {
+			// Direct array, probably expanded.
+			while (parser.nextToken() != JsonToken.END_ARRAY) {
+		        entities.add(parser.readValueAs(type));
+		    }
+		} else {
+			boolean done = false;
+			while (!done) {
+				JsonToken mainToken = parser.nextToken();
+				switch (mainToken) {
+				case END_OBJECT:
+					done = true;
+					break;
 
-                            case AT_IOT_NEXT_LINK:
-                                try {
-                                    entities.setNextLink(URI.create(parser.getValueAsString()));
-                                } catch (IllegalArgumentException e) {
-                                    LOGGER.warn("@iot.nextLink field contains malformed URI", e);
-                                }
-                                break;
+				case PROPERTY_NAME:
+					String fieldName = parser.currentName();
+					JsonToken valueToken = parser.nextToken();
+					switch (fieldName) {
+					case AT_IOT_COUNT:
+						entities.setCount(parser.getValueAsLong());
+						break;
 
-                            case "value":
-                                if (valueToken == JsonToken.START_ARRAY) {
-                                    JsonToken nextToken = parser.nextToken();
-                                    if (nextToken == JsonToken.END_ARRAY) {
-                                        // Empty array.
-                                    } else {
-                                        Iterator<T> values = parser.readValuesAs(type);
-                                        while (values.hasNext()) {
-                                            T value = values.next();
-                                            entities.add(value);
-                                        }
-                                    }
-                                } else {
-                                    LOGGER.warn("value field is not an array!");
-                                }
-                                break;
+					case AT_IOT_NEXT_LINK:
+						try {
+							entities.setNextLink(URI.create(parser.getValueAsString()));
+						} catch (IllegalArgumentException e) {
+							LOGGER.warn("@iot.nextLink field contains malformed URI", e);
+						}
+						break;
 
-                        }
-                        break;
+					case "value":
+						if (valueToken == JsonToken.START_ARRAY) {
+						    while (parser.nextToken() != JsonToken.END_ARRAY) {
+						        entities.add(parser.readValueAs(type));
+						    }
+						} else {
+							LOGGER.warn("value field is not an array!");
+						}
+						break;
 
-                    default:
-                        LOGGER.warn("Unhandled token: {}", mainToken);
-                }
-            }
-        }
-        return entities;
-    }
+					}
+					break;
+
+				default:
+					LOGGER.warn("Unhandled token: {}", mainToken);
+				}
+			}
+		}
+		return entities;
+	}
 
 }
