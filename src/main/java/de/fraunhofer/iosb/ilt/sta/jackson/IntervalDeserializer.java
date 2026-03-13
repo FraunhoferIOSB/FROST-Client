dbc2908d3c9a0a8147d22405d3178642963b07a1
j.sorg <j.sorg@fz-juelich.de>
2026-03-13 05:27:12 +0100

    removed no longer needed imports

--- src/main/java/de/fraunhofer/iosb/ilt/sta/jackson/IntervalDeserializer.java
+++ src/main/java/de/fraunhofer/iosb/ilt/sta/jackson/IntervalDeserializer.java
@@ -3,9 +3,7 @@ package de.fraunhofer.iosb.ilt.sta.jackson;
 import tools.jackson.core.JsonParser;
 import tools.jackson.core.JacksonException;
 import tools.jackson.databind.DeserializationContext;
-import tools.jackson.databind.JsonNode;
 import tools.jackson.databind.deser.std.StdDeserializer;
-import java.io.IOException;
 import org.threeten.extra.Interval;
 
 /**
